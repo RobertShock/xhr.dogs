0a38e78741efdea8db506897f6a62351d75f1e36
Robbie Shock <rob.f.shock@gmail.com>
2017-08-28 18:58:34 -0500

    fixed main.js

--- main.js
+++ main.js
@@ -8,11 +8,25 @@ function shitBroke (){
 	console.log("WHAT DID YOU DO?");
 };
 
-
-
-
 var myDawgs = new XMLHttpRequest;
 myDawgs.addEventListener("load", runThisAfterDogsLoads);
 myDawgs.addEventListener("error", shitBroke);
 myDawgs.open("GET", "dogs.json");
 myDawgs.send();
+
+
+
+function runThisAfterBreedsLoads(){
+	var data = JSON.parse(this.responseText).breeds;
+	console.log("breeds", data);
+};
+
+function stillBroke (){
+	console.log("NOW WHAT?");
+};
+
+var myDawgs2 = new XMLHttpRequest;
+myDawgs2.addEventListener("load", runThisAfterBreedsLoads);
+myDawgs2.addEventListener("error", stillBroke);
+myDawgs2.open("GET", "breeds.json");
+myDawgs2.send();
