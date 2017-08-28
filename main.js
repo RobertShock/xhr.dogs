37e6eb3b60f8b738c046e734c05142a07051c4f2
Robbie Shock <rob.f.shock@gmail.com>
2017-08-28 18:50:52 -0500

    added json text

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+
+function runThisAfterDogsLoads(){
+	var data = JSON.parse(this.responseText).dogs;
+	console.log("dogs", data);
+};
+
+function shitBroke (){
+	console.log("WHAT DID YOU DO?");
+};
+
+
+
+
 var myDawgs = new XMLHttpRequest;
+myDawgs.addEventListener("load", runThisAfterDogsLoads);
+myDawgs.addEventListener("error", shitBroke);
+myDawgs.open("GET", "dogs.json");
+myDawgs.send();
